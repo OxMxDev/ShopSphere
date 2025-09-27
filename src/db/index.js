d58c4159d76ee5c5463181718c563b884b1b1060
Om <omdwivedi234@gmail.com>
2025-09-27 17:05:38 +0530

    Add src folder with project files - removed embedded git repository

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+async function connectDB(){
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log('MONGODB CONNECTED !! DB HOST', connectionInstance.connection.host)
+    }catch(err){
+        console.error("MONGODB CONNECTION ERROR: ",err)
+    }
+}
+
+export default connectDB;
