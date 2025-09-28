a167a15a0876964aa299c0e8c08dcb306352d4ad
Om <omdwivedi234@gmail.com>
2025-09-28 19:13:55 +0530

    worked on setting up a production level backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
 import connectDB from './db/index.js';
-
+import app from './app.js';
 connectDB()
+.then(()=>{
+    app.on('error',(err)=>{
+        console.log("ERROR: ",err);
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.error("Database connection failed",err);
+})
