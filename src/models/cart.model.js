c8ed7476e183bf58b0ba4b8b90d6e441983cc740
Om <omdwivedi234@gmail.com>
2025-10-07 18:57:54 +0530

    created cart model

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,7 +1,17 @@
 import mongoose,{Schema} from "mongoose";
 
 const cartSchema = new Schema({
-
-})
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    items:[
+        {
+            product:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},
+            qty:{type:Number,required:true,min:1},
+        },
+    ],
+},{timestamps:true})
 
 export const Cart = mongoose.model('Cart',cartSchema)
