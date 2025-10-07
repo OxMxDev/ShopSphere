1c23b6453118ed64ec01c67bd0a8fce0d76bc237
Om <omdwivedi234@gmail.com>
2025-10-07 18:47:06 +0530

    completed user model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,7 +1,44 @@
 import mongoose,{Schema} from "mongoose";
 
 const productSchema = new Schema({
-
-})
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    brand:{
+        type:String
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    stock:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    images:[
+        {
+            url:String,
+            alt:String,
+        }
+    ],
+    ratings:{
+        type:Number,
+        default:0
+    },
+    numReviews:{
+        type:Number,
+        default:0
+    }
+},{timestamps:true})
 
 export const Product = mongoose.model("Product",productSchema)
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,8 +9,32 @@ const userSchema = new Schema({
     email:{
         type:String,
         required:true,
-        trim:true
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        requried:true,
+        minLength:6,
+    },
+    addresss:[
+        {
+            street:String,
+            city:String,
+            state:String,
+            postalCode:String,
+            country:String,
+            isDefault:{type:Boolean,default:false}
+        }
+    ],
+    phone:{
+        type:String,
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
     }
-})
+
+},{timestamps:true})
 
 export const User = mongoose.model("User",userSchema)
