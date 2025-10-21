a80afe1d8bef621f0e0918ab51d9842950b0c2a8
Om <omdwivedi234@gmail.com>
2025-10-21 11:44:24 +0530

    created getProductById function

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -58,8 +58,24 @@ const getAllProducts = asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200,products,"Products fetched successfully"))
 })
 
+const getProductById = asyncHandler(async(req,res)=>{
+    const {id} = req.params
+    if(!id){
+        throw new ApiError(400,"Product ID is required")
+    }
+    const product = await Product.findById(id)
+
+    if (!product) {
+		throw new ApiError(404, "Product not found");
+	}
+    return res
+    .status(200)
+    .json(new ApiResponse(200,product,"Product fetched successfully"))
+})
+
 export
 {
     createProduct,
-    getAllProducts
+    getAllProducts,
+    getProductById
 }
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createProduct, getAllProducts } from "../controllers/product.controller.js";
+import { createProduct, getAllProducts, getProductById } from "../controllers/product.controller.js";
 const router = Router()
 
 router.route('/create').post(
@@ -15,4 +15,5 @@ router.route('/create').post(
 )
 
 router.route('/getAllProducts').get(getAllProducts)
+router.route('/:id').get(getProductById)
 export default router
