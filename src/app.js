e66bea0f2f51eee9d6736b7014d8369b08d5ac5c
Om <omdwivedi234@gmail.com>
2025-11-10 22:29:44 +0530

    feat(wishlist): integrate wishlist routes in main application

--- src/app.js
+++ src/app.js
@@ -19,10 +19,12 @@ import adminRouter from "./routes/admin.route.js"
 import cartRouter from "./routes/cart.route.js"
 import orderRouter from "./routes/order.route.js"
 import categoryRouter from "./routes/category.route.js"
+import wishlistRouter from "./routes/wishlist.route.js"
 app.use('/api/v1/users',userRouter)
 app.use('/api/v1/products',productRouter)
 app.use('/api/v1/admins',adminRouter)
 app.use('/api/v1/cart',cartRouter)
 app.use('/api/v1/orders',orderRouter)
 app.use('/api/v1/categories',categoryRouter)
+app.use('/api/v1/wishlist',wishlistRouter)
 export {app}
