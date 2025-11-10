380e7d76420e525dd1d0eb7f662272d1c2fb3228
Om <omdwivedi234@gmail.com>
2025-11-10 18:15:28 +0530

    fix(cart): update routes to remove productId from add and remove cart endpoints

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 import { verifyAdmin } from "../middlewares/admin.middleware.js";
 import { addProductToCart, removeProductFromCart } from "../controllers/cart.controller.js";
-const router = Router()
+const router = Router() 
 
-router.route('/addToCart/:productId').post(verifyJWT,verifyAdmin,addProductToCart)
-router.route('/removeFromCart/:productId').post(verifyJWT,verifyAdmin,removeProductFromCart)
+router.route('/addToCart').post(verifyJWT,addProductToCart)
+router.route('/removeFromCart').post(verifyJWT,removeProductFromCart)
 
-export default router
+export default router 
