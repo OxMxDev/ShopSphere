9a56e77f80e5b4f2fec0233a53aad08555c65139
Om <omdwivedi234@gmail.com>
2025-12-18 23:32:09 +0530

    feat: create ProductCard component and refactor Products page to utilize it for product display

---
+++ frontend/e-commerce/src/components/product/ProductCard.jsx
@@ -0,0 +1,18 @@
+const ProductCard = ({ product }) => {
+	return (
+		<div className="border rounded-lg p-4 w-64">
+			<img
+				src={product.images?.[0]}
+				alt={product.name}
+				className="w-full h-60 object-cover rounded"
+			/>
+
+			<h3 className="font-bold mt-2">{product.name}</h3>
+			<p className="text-sm text-gray-600">{product.description}</p>
+
+			<p className="text-green-600 font-semibold mt-1">₹{product.price}</p>
+		</div>
+	);
+};
+
+export default ProductCard;
--- frontend/e-commerce/src/pages/Products.jsx
+++ frontend/e-commerce/src/pages/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getAllProducts } from "../api/product.api";
-
+import ProductCard from "../components/product/ProductCard";
 const Products = () => {
 	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(true);
@@ -23,17 +23,12 @@ const Products = () => {
 	if (loading) return <div>Loading...</div>;
 	if (error) return <div>{error}</div>;
 	return (
-		<div>
+		<div className="flex flex-wrap gap-4">
 			{products.length === 0 ? (
 				<p>No products found</p>
 			) : (
 				products.map((product) => (
-					<div key={product._id} className="border w-[20%] p-4 m-4 rounded-lg min-h-[300px]">
-                        <img className="w-full h-[60%] object-cover rounded" src={product.images[0]} alt={product.name} />
-						<p className="font-bold">{product.name}</p>
-						<p>{product.description}</p>
-						<p className="text-green-500 font-semibold">₹{product.price}</p>
-					</div>
+					<ProductCard key={product._id} product={product} />
 				))
 			)}
 		</div>
