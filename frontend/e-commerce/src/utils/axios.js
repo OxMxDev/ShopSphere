956624ae6c402c2bcd0ef6bef7b40aea3db056b6
Om <omdwivedi234@gmail.com>
2025-12-18 16:57:49 +0530

    feat: enhance axios instance with request and response interceptors for token management

--- frontend/e-commerce/src/utils/axios.js
+++ frontend/e-commerce/src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { config } from "dotenv"
 
 const axiosInstance = axios.create({
     baseURL:import.meta.env.VITE_BACKEND_URL,
@@ -6,3 +7,25 @@ const axiosInstance = axios.create({
 })
 
 export default axiosInstance
+
+axiosInstance.interceptors.request.use((config)=>{
+    const token = localStorage.getItem('accessToken')
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+},
+(error)=>{
+    return Promise.reject(error)
+})
+
+axiosInstance.interceptors.response.use((response)=>{
+    response,
+    (error)=>{
+        if(error.response.status==401){
+            localStorage.removeItem('token')
+            window.location.href='/login'
+        }
+        return Promise.reject(error)
+    }
+})
