9e16525998e011432c4cc28ba59285230b929256
Om <omdwivedi234@gmail.com>
2025-12-18 17:28:24 +0530

    feat: implement authentication API functions for login, registration, and user management

---
+++ frontend/e-commerce/src/api/auth.api.js
@@ -0,0 +1,15 @@
+import axiosInstance from "../utils/axios";
+
+export const loginUser = (data)=>{
+    return axiosInstance.post('/users/login',data)
+}
+
+export const registerUser = (data)=>{
+    return axiosInstance.post('/users/register',data)
+}
+export const getCurrentUser = ()=>{
+    return axiosInstance.get("/users/current-user");
+}
+export const logoutUser = ()=>{
+    localStorage.removeItem('token')
+}
